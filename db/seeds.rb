93e2d1e7c2723e391ff104c754f9b03c83bed786
skibaf <skiba_florencia@yahoo.com.ar>
2022-10-15 12:22:55 -0300

    seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+user1 = User.create! :nombre => 'Florencia', :email => 'skiba_flo@gmail.com', :password => '123456', :password_confirmation => '123456', :apellido => 'Skiba', :dni => '111111111', :direccion => 'guido', :localidad => 'Quilmes', :provincia => 'Bs As', :tipo => 'A', :admin => true
+user2 = User.create! :nombre=> 'Meli', :email => 'melii@gmail.com', :password => '123456', :password_confirmation => '123456', :apellido => 'Ja', :dni => '111111112', :direccion => 'guido', :localidad => 'Berazategui', :provincia => 'Bs As', :tipo => 'A'
+user3 = User.create! :nombre => 'Marti', :email => 'marti@gmail.com', :password => '123456', :password_confirmation => '123456', :apellido => 'Berraz', :dni => '111111113', :direccion => 'guido2', :localidad => 'Bernal', :provincia => 'Bs As', :tipo => 'A'
