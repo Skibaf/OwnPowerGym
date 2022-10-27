0b38441c5a8ebce60d8918ea8a72650641e78577
skibaf <skiba_florencia@yahoo.com.ar>
2022-10-27 20:30:06 -0300

    new cursos

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   get 'welcome/index'
+  get 'welcome/cursos'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
